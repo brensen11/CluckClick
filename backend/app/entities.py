7949304ff3c3e62d09795df7e68c3e2309e75e5f
brensen11 <90063365+brensen11@users.noreply.github.com>
2025-05-19 19:09:06 -0600

    entities added

--- backend/app/entities.py
+++ backend/app/entities.py
@@ -1,23 +1,70 @@
 from pydantic import BaseModel
 from typing import Optional
-from sqlmodel import SQLModel
+from sqlmodel import SQLModel, ForeignKey
 from fastapi import Query
+from datetime import datetime
 
 # ----------------------------------- #
 #            Request Models           #
 # ----------------------------------- #
+
+class UserCreate(BaseModel):
+    id: int
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = Query(None)
+    email: Optional[str] = Query(None)
+
+class ItemAdd(BaseModel):
+    name: str
+    image: str # TODO correct? image/file upload ... ?
+
+class ItemUpdate(BaseModel):
+    id: int
+    name: str
+    image: str
+
+class ItemRemove(BaseModel):
+    id: int
+
+class ClickAdd(BaseModel):
+    item_id: int
+
+class ClickRemove(BaseModel):
+    click_id: int
 
 
 # ----------------------------------- #
 #             Data Models             #
 # ----------------------------------- #
 
+class User(SQLModel):
+    """Represents a User in the DB and as returned by the API"""
+    id: int
+    username: str
+    email: str
+    created_at: datetime
+
+
 class Item(SQLModel):
+    """Represents an Item a user can track"""
     id: int
     name: str
     image: str
+    user: User
+
+class Click(SQLModel):
+    """Represents 1 click of an item and when it occurred"""
+    id: int
+    clicked_at: datetime
+    item: Item
 
 
 # ----------------------------------- #
 #           Response Models           #
 # ----------------------------------- #
+
+class ItemCollection(BaseModel):
+    """Represents all Items associated with a user"""
+    items: list[Item]
+    user: ForeignKey[User]
