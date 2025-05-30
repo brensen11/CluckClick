7dbba70215acba22ebdf5c54f517306b43d54c28
Brensen Villegas <brensen11@gmail.com>
2025-05-29 23:37:10 -0600

    adjusting formatting

--- backend/app/entities.py
+++ backend/app/entities.py
@@ -1,42 +1,17 @@
 from pydantic import BaseModel
 from typing import Optional
-from sqlmodel import SQLModel, ForeignKey
+from sqlmodel import SQLModel, Field
 from fastapi import Query
 from datetime import datetime
 
 # ----------------------------------- #
-#            Request Models           #
-# ----------------------------------- #
-
-class UserCreate(BaseModel):
-    id: int
-
-class UserUpdate(BaseModel):
-    username: Optional[str] = Query(None)
-    email: Optional[str] = Query(None)
-
-class ItemCreate(BaseModel):
-    name: str
-    image: str # TODO correct? image/file upload ... ?
-
-class ItemUpdate(BaseModel):
-    id: int
-    name: Optional[str]
-    image: Optional[str]
-
-class ItemDelete(BaseModel):
-    id: int
-
-class ClickCreate(BaseModel):
-    item_id: int
-
-class ClickDelete(BaseModel):
-    click_id: int
-
-
-# ----------------------------------- #
 #             Data Models             #
 # ----------------------------------- #
+#
+# Models which are used in requests or responses.
+# Inherit from SQLModel for ease of casting from SQLModels
+# As many of them are slimmer versions of SQLModels
+#
 
 class User(SQLModel):
     """Represents a User in the DB and as returned by the API"""
@@ -51,7 +26,7 @@ class Item(SQLModel):
     id: int
     name: str
     image: str
-    user: User
+
 
 class Click(SQLModel):
     """Represents 1 click of an item and when it occurred"""
@@ -61,23 +36,57 @@ class Click(SQLModel):
 
 
 # ----------------------------------- #
+#            Request Models           #
+# ----------------------------------- #
+# 
+# Models for Requests, meant to be used in routers
+# 
+
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = Query(None)
+    email: Optional[str] = Query(None)
+
+
+class ItemCreate(BaseModel):
+    name: str
+    image: str # TODO correct? image/file upload ... ?
+    user: int
+
+
+class ItemUpdate(BaseModel):
+    name: Optional[str]
+    image: Optional[str]
+
+
+# ----------------------------------- #
 #           Response Models           #
 # ----------------------------------- #
+# 
+# Models for Responses, meant to be used in routers
+# 
 
 class ItemResponse(BaseModel):
     item: Item
 
+
 class UserResponse(BaseModel):
     user: User
+
 
 class ClickResponse(BaseModel):
     click: Click
 
+
 class ItemCollection(BaseModel):
     """Represents all Items associated with a user"""
     items: list[Item]
-    user_id: int
+
 
 class ClickCollection(BaseModel):
     clicks: list[Click]
-    user_id: int
